efc8c7f59c3fefdffb79b6c2d30dbd8ebef2952e
OtavioHFerrazDeJesus <otavioh.ferrazdejesus@gmail.com>
2025-11-26 14:17:03 -0300

    mucho texto

---
+++ js.js
@@ -0,0 +1,7 @@
+document.getElementById("git").addEventListener("click",function(){
+    window.open("Github_basico/index.html",)
+})
+document.getElementById("jscpt").addEventListener("click",function(){
+    window.open("JS_programacao/index.html")
+})
+
