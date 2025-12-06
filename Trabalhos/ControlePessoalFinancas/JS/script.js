09848a6faf5a769ac85a52a18b794e15fbb9fc12
Otavio H. Ferraz <otavioh.ferrazdejesus@gmail.com>
2025-12-06 13:55:13 -0300

    Lógica completa

--- Trabalhos/ControlePessoalFinancas/JS/script.js
+++ Trabalhos/ControlePessoalFinancas/JS/script.js
@@ -1,13 +1,64 @@
-document.getElementById("formSalario").addEventListener("submit",function(){
-     var salario = document.getElementById("salario").value
-     var teste = JSON.parse(localStorage.getItem('teste'))||[]
+document.getElementById('formSalario').addEventListener('submit', function(event){
+    event.preventDefault()
 
-     teste.push(salario)
+    var salario = parseFloat(document.getElementById('salario').value)
 
-     localStorage.setItem(JSON.stringify(teste))
+    var valores = JSON.parse(localStorage.getItem('valores'))||[0,0,0]
 
-     mostrarTestes()
+    valores[0] = valores[0]+salario//define o saldo disponivel
+
+    localStorage.setItem('valores', JSON.stringify(valores))
+
+    mostrarValores()
 })
-function mostrarTestes(){
+document.getElementById('formDespesas').addEventListener('submit',function(event){
+    event.preventDefault()
+
+    //adicionando uma despesa na lista
+
+    var valor = parseFloat(document.getElementById('valor').value)
+    var nome = document.getElementById('nome').value
+    var data = (document.getElementById('data').value).split('-')
+
+    data = data[2]+"-"+data[1]+"-"+data[0]
+
+    despesa = {valor:valor,nome:nome,data:data}
     
+    var despesas = JSON.parse(localStorage.getItem('despesas'))||[]
+    despesas.push(despesa)
+    
+    localStorage.setItem('despesas',JSON.stringify(despesas))
+    
+    //atualizando o total de despesas
+    
+    var valores = JSON.parse(localStorage.getItem('valores'))||[0,0,0]
+    
+    valores[1] = valores[1]+valor
+    
+    
+    //atualizando o saldo total
+    
+    valores[2] = valores[0]-valores[1]
+
+    alert(valores[2])
+    
+    
+    localStorage.setItem('valores',JSON.stringify(valores))
+})
+
+function mostrarValores(){
+
 }
+
+document.getElementById('btnClear').addEventListener('click',function(event){
+    //limpa os arquivos json(usar apenas durante o desenvolvimento)
+
+    confirmacao = confirm('Está ação ira apagar todos os dados salvos dentro do localStorage. Tem certeza desta ação?')
+    
+    if(confirmacao){
+
+        localStorage.removeItem('valores')
+        localStorage.removeItem('despesas')
+    }
+    mostrarValores()
+})
