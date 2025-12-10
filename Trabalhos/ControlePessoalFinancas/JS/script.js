d4747d04af5cfb52ea52a9cc26a678f78a48b2ad
Otavio H. Ferraz <otavioh.ferrazdejesus@gmail.com>
2025-12-10 12:36:32 -0300

    CSS, melhorias no js, index formatado para css e um icon

--- Trabalhos/ControlePessoalFinancas/JS/script.js
+++ Trabalhos/ControlePessoalFinancas/JS/script.js
@@ -5,56 +5,74 @@ document.getElementById('formSalario').addEventListener('submit', function(event
 
     var valores = JSON.parse(localStorage.getItem('valores'))||[0,0,0]
 
-    valores[0] = valores[0]+salario//define o saldo disponivel
+    valores[0] = salario//define o ultimo salário adicionado
+
+    valores[2] = valores[2]+salario//atualiza o saldo total
+
+    localStorage.setItem('valores', JSON.stringify(valores))
+
+    document.getElementById('formSalario').reset()
+
+    mostrarValores()
+})
+document.getElementById("formDespesas").addEventListener("submit",function(event){
+    event.preventDefault()
+
+    var valor = parseFloat(document.getElementById('valor').value)
+    var nome = document.getElementById('nome').value
+    var data = document.getElementById('data').value.split('-')
+
+    data = data[2]+'-'+data[1]+'-'+data[0]
+
+    despesa = {valor:valor,nome:nome,data:data}
+
+    var despesas = JSON.parse(localStorage.getItem("despesas"))||[]
+    despesas.push(despesa)
+
+    localStorage.setItem('despesas', JSON.stringify(despesas))
+
+    var valores = JSON.parse(localStorage.getItem('valores'))||[0,0,0]
+
+    valores[1] = valores[1]+valor
+
+    valores[2] = valores[2]-valor
 
     localStorage.setItem('valores', JSON.stringify(valores))
 
     mostrarValores()
+
+
 })
-document.getElementById('formDespesas').addEventListener('submit',function(event){
-    event.preventDefault()
-
-    //adicionando uma despesa na lista
-
-    var valor = parseFloat(document.getElementById('valor').value)
-    var nome = document.getElementById('nome').value
-    var data = (document.getElementById('data').value).split('-')
-
-    data = data[2]+"-"+data[1]+"-"+data[0]
-
-    despesa = {valor:valor,nome:nome,data:data}
-    
-    var despesas = JSON.parse(localStorage.getItem('despesas'))||[]
-    despesas.push(despesa)
-    
-    localStorage.setItem('despesas',JSON.stringify(despesas))
-    
-    //atualizando o total de despesas
-    
-    valores[1] = valores[1]+valor
-    
-    
-    //atualizando o saldo total
-    
-    valores[2] = valores[0]-valores[1]
-
-    localStorage.setItem('valores',JSON.stringify(valores))
-
-    mostrarValores()
-})
-
 
 function mostrarValores(){
     var valores = JSON.parse(localStorage.getItem('valores'))||[0,0,0]
     var despesas = JSON.parse(localStorage.getItem('despesas'))||[]
-    var output = document.getElementById('despesas')
-
+    var ulDespesas = document.getElementById('despesas')
+    var ulValores = document.getElementById('valores')
+    
     for(let i=0;i<despesas.length;i++){
         let li = document.createElement('li')
         li.textContent = despesas[i].valor+" "+despesas[i].nome+" "+despesas[i].data
-        output.appendChild(li)
+        ulDespesas.appendChild(li)
     }
+    salario = document.createElement('li')
+    salario.textContent = "Salário: "+valores[0]
+    
+    despesasTotais = document.createElement('li')
+    despesasTotais.textContent = "Despesas Totais:"+valores[1]
+    
+    saldoFinal = document.createElement('li')
+    saldoFinal.textContent = "Despesas Totais:"+valores[2]
+    
+    ulValores.innerHTML=""
+    
+    ulValores.appendChild(salario)
+    ulValores.appendChild(despesasTotais)
+    ulValores.appendChild(saldoFinal)
 }
+    
+    
+    
 
 document.getElementById('btnClear').addEventListener('click',function(event){
     //limpa os arquivos json(usar apenas durante o desenvolvimento)
@@ -65,8 +83,10 @@ document.getElementById('btnClear').addEventListener('click',function(event){
 
         localStorage.removeItem('valores')
         localStorage.removeItem('despesas')
+
+        mostrarValores()
         
     }
-    return mostrarValores()
+    
 })
 mostrarValores()
